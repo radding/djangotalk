3d56d72268bdf18f774f0aff7692901d51bd7c82
Yoseph Radding <raddingy@lykeme.com>
2015-03-26 02:18:52 -0400

    Added git ignore

---
+++ polls/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from polls.views import index, voteUpAns, viewAllPolls, viewPoll, viewPoll2
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'djangotalk.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^view/all/', viewAllPolls),
+    url(r'^view/(?P<pollID>\d+)/$', viewPoll),
+    url(r'^view/2/(?P<pollID>\d+)/$', viewPoll2),
+    url(r'^vote/(?P<ansID>\d+)/$', voteUpAns),
+    url(r'/', index),
+]
+
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from polls.models import Question, Answer
+from django.http import HttpResponse
+from django.http import Http404
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def viewAllPolls(request):
+    questions = Question.objects.all()
+    return render(request, "polls/all.html",{"questions":questions})
+
+def viewPoll(request, pollID):
+    try:
+        question = Question.objects.get(pk=pollID)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    answers = Answer.objects.filter(question = question)
+    return render(request, "polls/one.html",{"questions":question,"ans":answers})
+
+def viewPoll2(request, pollID):
+    question = get_object_or_404(Question, pk=pollID)
+    answers = Answer.objects.filter(question = question)
+    return render(request, "polls/one.html",{"questions":question,"ans":answers})
+
+def voteUpAns(request,ansID):
+    answer = question = get_object_or_404(Question, pk=ansID)
+    answer.votes += 1
+    answer.save()
+    return render(request, "polls/vote.html", {"ques":answer.question, "ans":answer})
